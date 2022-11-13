83dae1d4029e06efe8a2cc24011803236e45c85a
sarahhlandis <sarahlandis95@gmail.com>
2022-11-13 12:08:45 +1100

    organized all files, adjusted links. polished .md, tweaked code for queries.

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-function nightMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    if(element.classList.contains("dark-mode")){
-       localStorage.setItem("toggle","yes");
-    }else{
-       localStorage.setItem("toggle","no");
-    }
-   }
